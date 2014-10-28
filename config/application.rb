c766f4f75331c24da131e1fc2b0b5e9ee68779ac
Hannes Wüthrich <hw@5px.ch>
2014-10-28 17:54:56 +0800

    Switched default locale to english

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Scoobar
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # config.i18n.available_locales = [:en, :de, :fr]
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     config.generators do |g|
       g.test_framework :rspec, fixture: false
