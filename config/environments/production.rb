055f8f1b15221b0aa8c7cd11c26e37ff7a312ed1
Hannes Wüthrich <hw@5px.ch>
2014-10-28 18:09:37 +0800

    Make assets precopilation work on dokku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # i18n-js requires environment for compiling
+  config.assets.initialize_on_precompile = true
 end
