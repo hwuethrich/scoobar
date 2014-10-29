3cc47577b43440509a4268aa25a8c074c8ff48fc
Hannes Wüthrich <hw@5px.ch>
2014-10-29 21:41:36 +0800

    Formatting

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -39,7 +39,11 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :date_of_birth, :gender, :email, :phone_number, :address1, :address2, :city, :state, :post_code, :country, :certification_name, :certification_number, :certification_date, :last_dive_on, :number_of_dives)
+    params.require(:customer).permit(
+      :first_name, :last_name, :date_of_birth, :gender,
+      :email, :phone_number, :address1, :address2, :city, :state, :post_code, :country,
+      :certification_name, :certification_number, :certification_date, :last_dive_on, :number_of_dives
+    )
   end
 
 end
