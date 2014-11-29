49827054ce1ea857d4147f7b2acd323e61599d2c
Hannes Wüthrich <hw@5px.ch>
2014-11-29 16:58:11 +0800

    Capybara screenshot config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,12 @@
 require 'capybara/rspec'
 require 'capybara/poltergeist'
+require 'capybara-screenshot/rspec'
 
+# Use poltergeist for browser testing
 Capybara.javascript_driver = :poltergeist
+
+Capybara::Screenshot.autosave_on_failure = true
+
+# Keep only the screenshots generated from the last failing test suite
+Capybara::Screenshot.prune_strategy = :keep_last_run
+
