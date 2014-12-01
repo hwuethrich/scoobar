557b24f5c27b9714d634786182add1a2e3494f4b
Hannes Wüthrich <hw@5px.ch>
2014-12-01 11:52:47 +0800

    Clean up views

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,6 +1,6 @@
 module LinkHelper
   def action_links_for(model, *actions)
-    actions ||= [:edit, :delete]
+    actions = [:edit, :delete] if actions.blank?
     render partial: 'common/actions', locals: { model: model, actions: actions }
   end
 end
