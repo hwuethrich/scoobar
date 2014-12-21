67639affc3bc82869dbd94ff201f648bcf48e2cb
Hannes Wüthrich <hw@5px.ch>
2014-12-21 18:30:08 +0800

    Add bookings for customers

--- app/helpers/trip_helper.rb
+++ app/helpers/trip_helper.rb
@@ -3,11 +3,19 @@ module TripHelper
   def trip_code_and_name(trip)
     capture_haml do
       if trip.present?
-        style = 'background-color: %s;' % trip.color
-        haml_tag :span, trip.code, class: 'label label-default trip-code', style: style
+        haml_concat trip_code trip
         haml_tag :span, trip.name, class: 'trip-name'
       else
         placeholder
+      end
+    end
+  end
+
+  def trip_code(trip)
+    capture_haml do
+      if trip.present?
+        style = 'background-color: %s;' % trip.color
+        haml_tag :span, trip.code, class: 'label label-default trip-code', style: style
       end
     end
   end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,4 +8,8 @@ class Trip < ActiveRecord::Base
   validates :duration, numericality: { greater_than: 0, allow_nil: true }
   validates :price, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
 
+  def to_s
+    name
+  end
+
 end
