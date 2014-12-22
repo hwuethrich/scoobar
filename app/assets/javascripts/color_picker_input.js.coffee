ebf58116373d26dd0f826979f1cf3946b101d0d0
Hannes Wüthrich <hw@5px.ch>
2014-12-22 23:45:53 +0800

    Update colorpicker, show color in input addon

--- app/assets/javascripts/color_picker_input.js.coffee
+++ app/assets/javascripts/color_picker_input.js.coffee
@@ -1,2 +1,2 @@
 $(document).on 'page:change', ->
-  $("input[rel=colorpicker]").colorpicker()
+  $("[rel=colorpicker]").colorpicker()
--- app/inputs/color_picker_input.rb
+++ app/inputs/color_picker_input.rb
@@ -1,6 +1,12 @@
 class ColorPickerInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options = nil)
-    input_html_options[:rel] = 'colorpicker'
-    super
+    template.capture_haml do
+      template.haml_tag :span, class: 'input-group', rel: 'colorpicker' do
+        template.haml_tag :span, class: 'input-group-addon' do
+          template.haml_tag :i
+        end
+        template.haml_concat super
+      end
+    end
   end
 end
